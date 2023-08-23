8992330967592559e5f1ed20da39bab47bc2b2f9
Sofia <sofiahgelado@gmail.com>
2023-08-23 14:50:16 +0100

    Restructuring files in repo

--- SRCNN/run.py
+++ SRCNN/run.py
@@ -26,39 +26,14 @@ eval_file = eval_dir + "/eval_file.h5"
 --scale 8 --eval
 
 
-#"/home/daniel/Documents/SRCNN_pytorch2/train/train_file.h5"
-#"/home/daniel/Documents/SRCNN_pytorch2/eval/eval_file.h5"
-#/home/daniel/Documents/SRCNN_pytorch2/Experiments/Exp4/outputs"
-
 !python /home/daniel/Documents/SRCNN_pytorch2/train.py\
     --train-file "/home/daniel/Documents/SRCNN_pytorch2/train/train_file.h5" \
                 --eval-file "/home/daniel/Documents/SRCNN_pytorch2/eval/eval_file.h5" \
                 --outputs-dir "/home/daniel/Documents/SRCNN_pytorch2/Experiments/Exp6/outputs" \
-                --scale 2 \
-                --lr 1e-6 \
+                --scale 8 \
+                --lr 1e-5 \
                 --batch-size 32\
                 --num-epochs 100\
                 --num-workers 2 \
                 --seed 123
-                
-             
-               
-!python /home/daniel/Documents/SRCNN_pytorch2/test.py \
---weights-file "/home/daniel/Documents/SRCNN_pytorch2/Experiments/Exp7/outputs/x4/epoch_723.pth" \
---image-dir "/home/daniel/Documents/SRCNN_pytorch2/Experiments/Exp7/test" \
---scale 4
-               
-
-
-!python /home/daniel/Documents/SRCNN_pytorch2/test.py \
---weights-file "/home/daniel/Documents/Test1/outputs/x4/best.pth" \
---image-dir "/home/daniel/Documents/Test1/test" \
---scale 4
-            
-
-                
-import torch 
-torch.cuda.is_available()
-
-
-!python /home/daniel/Documents/pytorch-vdsr/main_vdsr.py --cuda
+                           
