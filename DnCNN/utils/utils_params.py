39938b963f2b4589779e80f41876643881dbf6f4
Sofia Hernandez Gelado <87440853+SoFia2401@users.noreply.github.com>
2023-09-10 23:30:40 +0100

    Update utils_params.py

--- DnCNN/utils/utils_params.py
+++ DnCNN/utils/utils_params.py
@@ -8,26 +8,6 @@ def show_kv(net):
     for k, v in net.items():
         print(k)
 
-# should run train debug mode first to get an initial model
-#crt_net = torch.load('../../experiments/debug_SRResNet_bicx4_in3nf64nb16/models/8_G.pth')
-#
-#for k, v in crt_net.items():
-#    print(k)
-#for k, v in crt_net.items():
-#    if k in pretrained_net:
-#        crt_net[k] = pretrained_net[k]
-#        print('replace ... ', k)
-
-# x2 -> x4
-#crt_net['model.5.weight'] = pretrained_net['model.2.weight']
-#crt_net['model.5.bias'] = pretrained_net['model.2.bias']
-#crt_net['model.8.weight'] = pretrained_net['model.5.weight']
-#crt_net['model.8.bias'] = pretrained_net['model.5.bias']
-#crt_net['model.10.weight'] = pretrained_net['model.7.weight']
-#crt_net['model.10.bias'] = pretrained_net['model.7.bias']
-#torch.save(crt_net, '../pretrained_tmp.pth')
-
-# x2 -> x3
 '''
 in_filter = pretrained_net['model.2.weight'] # 256, 64, 3, 3
 new_filter = torch.Tensor(576, 64, 3, 3)
@@ -68,18 +48,6 @@ def rgb2gray_net(net, only_input=True):
         in_new_filter = in_filter[:,0,:,:]*0.2989 + in_filter[:,1,:,:]*0.587 + in_filter[:,2,:,:]*0.114
         in_new_filter.unsqueeze_(1)
         net['0.weight'] = in_new_filter
-
-#    out_filter = pretrained_net['model.13.weight']
-#    out_new_filter = out_filter[0, :, :, :] * 0.2989 + out_filter[1, :, :, :] * 0.587 + \
-#        out_filter[2, :, :, :] * 0.114
-#    out_new_filter.unsqueeze_(0)
-#    crt_net['model.13.weight'] = out_new_filter
-#    out_bias = pretrained_net['model.13.bias']
-#    out_new_bias = out_bias[0] * 0.2989 + out_bias[1] * 0.587 + out_bias[2] * 0.114
-#    out_new_bias = torch.Tensor(1).fill_(out_new_bias)
-#    crt_net['model.13.bias'] = out_new_bias
-
-#    torch.save(crt_net, '../pretrained_tmp.pth')
 
     return net
 
